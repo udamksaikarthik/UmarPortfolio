26fb38d8ab436dd817fe31c3b093459404548f39
udamksaikarthik <50781377+udamksaikarthik@users.noreply.github.com>
2023-03-11 06:06:27 +0530

    new changes

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ function sliderMenu(){
     slider_menu_div_id.classList.toggle('active')
     setTimeout(()=>{main_container_id.classList.toggle('active')},500)
 }
+
+function home(){
+    setTimeout(()=>{window.location.href='#home'},500)
+}
+function skills(){
+    setTimeout(()=>{window.location.href='#skills'},500)
+}
+function certifications(){
+    setTimeout(()=>{window.location.href='#certifications'},500)
+}
+function journey(){
+    setTimeout(()=>{window.location.href='#journey'},500)
+}
+function contact(){
+    setTimeout(()=>{window.location.href='#contact'},500)
+}
